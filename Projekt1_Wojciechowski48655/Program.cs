e0fd84474fd930638b0723032e5f6fb3f362bf6f
Mateusz Wojciechowski <mattchowski@gmail.com>
2018-10-16 15:45:20 +0200

    asd

--- Projekt1_Wojciechowski48655/Program.cs
+++ Projekt1_Wojciechowski48655/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Projekt1_Wojciechowski48655 {
 	class Program {
 		static void Main(string[] args) {
+			var myString = "Hey there" + Environment.NewLine + "How are you doing?";
+
+			foreach (var character in myString) {
+				Console.Write(character);
+				System.Threading.Thread.Sleep(30);
+			}
+			Console.WriteLine();
+			Console.ReadLine();
 		}
 	}
 }
